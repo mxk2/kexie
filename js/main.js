928818b7658daa54398b71b318e968b1f8a38d61
mengxk <mengxk@yntrust.com>
2015-10-21 09:02:34 +0800

    add

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
-requirejs.config({
-	baseUrl: "../js/lib",
-	paths: {
-		app: "../app",
-		jquery: "jquery-1.11.3.min",
-		bootstrap: "bootstrap.min"
-	},
-	shim:{
-		bootstrap:{
-			deps: ["jquery"]
+$(document).ready(function(){
+	//输入框
+	$("body").on("focus", ".box-form .form-control", function(){
+		var $parent = $(this).closest(".input-group");
+		$(".input-group-focus").removeClass("input-group-focus");
+		$(".error-title").css("visibility", "hidden");
+		$parent.addClass("input-group-focus");
+	});
+	$("body").on("blur", ".box-form .form-control", function(){
+		var $parent = $(this).closest(".input-group");
+		$(".input-group-focus").removeClass("input-group-focus");
+		$parent.addClass("input-group-focus");
+		$(".error-title").css("visibility", "hidden");
+		if($(this).val().length == 0){
+			$parent.next(".error-title").css("visibility", "visible");
 		}
-	}
-});
-
-requirejs(["jquery", "bootstrap", "app/index"], function($, b, index){
-	console.log($);
-	console.log(b);
-	console.log(index);
+	});
+	//btn-circle
+	$("body").on("mouseover", ".btn-circle", function(){
+		$(this).addClass($(this).attr("data-hover"));
+	}).on("mouseout", ".btn-circle", function(){
+		$(this).removeClass($(this).attr("data-hover"));
+	});
 });
